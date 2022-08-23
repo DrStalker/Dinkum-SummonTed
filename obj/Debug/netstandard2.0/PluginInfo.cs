6e8b5b2d55cdcbee12cb0940e8508ec3df525a78
Doug <doug.stalker@gmail.com>
2022-08-23 15:18:55 +1000

    .gitignore implemented

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SummonTed
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "Nep.SummonTed";
-        public const string PLUGIN_NAME = "Nep.SummonTed";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
