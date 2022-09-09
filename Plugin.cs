63d442e996c57d94f7afcfb25f1735f6e018a867
Doug <doug.stalker@gmail.com>
2022-09-09 18:16:10 +1000

    give direction to Ted if already on the island.

--- Plugin.cs
+++ Plugin.cs
@@ -30,9 +30,17 @@ public class Plugin : BaseUnityPlugin
         if (Input.GetKeyDown(_hotKey.Value))
         {
             Logger.LogInfo($"Attempting to summon Ted");
-            int playerX=0;
-            int playerY=0;
+            float playerX=0;
+            float playerY=0;
+            float tedX=0;
+            float tedY=0;
+            float dX=0;
+            float dY=0;
+            float tedDistance=0;
+            float tedDirection=0;
             bool iFoundTed=false;
+            string tedDirName="UnknownDirection";
+            int tedNPCId = -1;
 
             // Is Ted already here?
             for (int i = 0; i < NPCManager.manage.npcsOnMap.Count; i++)
@@ -40,7 +48,8 @@ public class Plugin : BaseUnityPlugin
                 if (NPCManager.manage.npcsOnMap[i].npcId==5)
                 {
                     iFoundTed=true;
-                    NotificationManager.manage.createChatNotification("Ted Selly is already somewhere on the island");
+                    tedNPCId=i;
+
                 }
             }
 
@@ -56,12 +65,50 @@ public class Plugin : BaseUnityPlugin
                 playerY=(int)(NetworkMapSharer.share.localChar.transform.position.z/2);
 
                 //spawn NPC & play the Ted Selly whistle
-                NPCManager.manage.npcsOnMap.Add(new NPCMapAgent(5, playerX, playerY));
+                NPCManager.manage.npcsOnMap.Add(new NPCMapAgent(5, (int)playerX, (int)playerY));
                 NetworkMapSharer.share.RpcPlayTrapperSound(new Vector3((float)(playerX * 2), 0f, (float)(playerY * 2)));
                 
                 NotificationManager.manage.createChatNotification(String.Format("Ted Selly has been summoned!"));
             }
-            // TODO: else move Ted to player's position.
+            else //describe where Ted is, relative to the player
+            {
+                //NotificationManager.manage.createChatNotification("Ted Selly is already somewhere on the island!");
+                //get your location, ted's location, work out the distance and direction.
+                //this can probably be done better using Unity's vector3 class functions instead of pullingout the components like this, but given how weirdly the vector3 class gets used this requires a lot less effort to get working.
+                tedX=(int)(NPCManager.manage.npcsOnMap[tedNPCId].currentPosition.x/2);
+                tedY=(int)(NPCManager.manage.npcsOnMap[tedNPCId].currentPosition.z/2);
+                playerX=(int)(NetworkMapSharer.share.localChar.transform.position.x/2);
+                playerY=(int)(NetworkMapSharer.share.localChar.transform.position.z/2);
+                dX=tedX-playerX;
+                dY=tedY-playerY;
+                tedDistance=(int)Math.Sqrt((double)((dX*dX)+(dY*dY)));  
+                tedDirection=(int)((Math.Atan(Math.Abs((double)dY/(double)dX)))*(180/Math.PI));  //east =0°. Get angle to east/west axis then adjust  based on the quadrent.
+                if (dX<0 && dY>=0) {tedDirection=180f-tedDirection;}
+                if (dX<0 && dY<0) {tedDirection=tedDirection+180f;}
+                if (dX>0 && dY<0) {tedDirection=360f-tedDirection;}
+               
+                // TO DO: make this an enum and a loop instead of this mess, and also work in NNE style directions.
+                if (tedDirection<22.5f) {tedDirName="east";}
+                else if (tedDirection<(22.5f+(1f*45f))) {tedDirName="northeast";}
+                else if (tedDirection<(22.5f+(2f*45f))) {tedDirName="north";}
+                else if (tedDirection<(22.5f+(3f*45f))) {tedDirName="northwest";}
+                else if (tedDirection<(22.5f+(4f*45f))) {tedDirName="west";}
+                else if (tedDirection<(22.5f+(5f*45f))) {tedDirName="southwest";}
+                else if (tedDirection<(22.5f+(6f*45f))) {tedDirName="south";}
+                else if (tedDirection<(22.5f+(7f*45f))) {tedDirName="southeast";}
+                else  {tedDirName="east";}
+
+                //Now tell the player where he is
+                if (tedDistance<=5)
+                {
+                    NotificationManager.manage.createChatNotification("Ted Selly is very close.");                        
+                }
+                else
+                {
+                    NotificationManager.manage.createChatNotification(String.Format("Ted Selly is {0} metres to the {1}.",tedDistance,tedDirName));         
+                }
+
+            }
 
             
         }
